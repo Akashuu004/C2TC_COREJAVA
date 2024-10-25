a72e2223f45640cbdebf764f7fd3af2d02158c60
Akash <Akash@LAPTOP-UNHE7MK7>
2024-10-25 16:13:17 +0530

    programs

---
+++ C2TC_AKASH_M/src/com/tns/ifet/day2/IdentifierDemo.java
@@ -0,0 +1,14 @@
+package com.tns.ifet.day2;
+
+public class IdentifierDemo {
+
+	public static void main(String[] args) {
+		int $number = 77; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+		String studentName="Akash"; //use camelcase
+		System.out.println("value is : "+studentName);
+		}
+}
+
+		
+	
