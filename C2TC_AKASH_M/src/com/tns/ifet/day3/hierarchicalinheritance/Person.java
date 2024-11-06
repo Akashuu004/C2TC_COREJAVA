72fbd1ebfde7d1f3c65daf943251e7e4b7560560
Akash <Akash@LAPTOP-UNHE7MK7>
2024-11-06 09:44:14 +0530

    uploaded code

---
+++ C2TC_AKASH_M/src/com/tns/ifet/day3/hierarchicalinheritance/Person.java
@@ -0,0 +1,26 @@
+package com.tns.ifet.day3.hierarchicalinheritance;
+
+public class Person {
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", city=" + city + "]";
+	}
+
+	
+
+}
+
+
+
